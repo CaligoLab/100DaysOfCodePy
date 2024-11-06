01fc43f29ae97d2599e355606df48d0df6adf329
TA <caligolab@gmail.com>
2024-11-06 11:26:42 +0100

    Email automation - birthday wisher. Initial version. todo: App Pass from mail

---
+++ 32_email-automation_birthday-wisher/main.py
@@ -0,0 +1,33 @@
+
+# TODO: Add App Password from gmail (waiting for the option to appear after turning on 2FA - or need troubleshooting)
+
+from datetime import datetime
+import pandas
+import random
+import smtplib
+
+from config.config import EMAIL, PASSWORD, SMTP_SERVER
+
+MY_EMAIL = EMAIL
+MY_PASSWORD = PASSWORD
+
+today = datetime.now()
+today_tuple = (today.month, today.day)
+
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP(SMTP_SERVER) as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!\n\n{contents}"
+        )
